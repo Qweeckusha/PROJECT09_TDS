ce134bd9af8d427d88fe3abc941b32c323eeb4c0
qweeck <tolya.zaytsev.2004@gmail.com>
2026-06-24 09:24:01 +1000

    added preprocessing audio files

--- src/transcriber.py
+++ src/transcriber.py
@@ -1,5 +1,7 @@
 from faster_whisper import WhisperModel
 from pathlib import Path
+import numpy as np
+import subprocess
 
 import torch
 print(torch.__version__)
@@ -33,6 +35,32 @@ class Transcriber:
             compute_type=compute_type
         )
 
+    def _load_and_preprocess(self, path: str) -> np.ndarray:
+        """
+        Обрабатывает файл по контракту faster-whisper перед транскрибацией и загружает в память
+
+        :param path: путь к аудио фвйлу
+        :return: numpy array формы (N,) [-1.0, 1.0]
+        """
+
+        cmd = [
+            "ffmpeg",
+            "-i", str(path),
+            "-ar", "16000",
+            "-ac", "1",
+            "-f", "f32le",
+            "-acodec", "pcm_f32le",
+            "pipe:1"
+        ]
+
+        proccess = subprocess.run(
+            cmd,
+            capture_output=True,
+            check=True,
+        )
+
+        return np.frombuffer(proccess.stdout, dtype=np.float32)
+
     def transcribe_audio(self,
                          audio_path: str | Path,
                          language: str = "ru"
@@ -47,9 +75,10 @@ class Transcriber:
            list[dict]: Список сегментов вида:
                 [{"start": 0.0, "end": 5.42, "text": "Привет!"}, ...]
         """
+        audio = self._load_and_preprocess(audio_path)
 
         segments, info = self.model.transcribe(
-            audio_path,
+            audio,
             language=language,
             beam_size=5
         )
@@ -61,16 +90,15 @@ class Transcriber:
                 "end": segment.end,
                 "text": segment.text.strip()
             })
-            print(f"[{segment.start:.2f}s -> {segment.end:.2f}s] {segment.text}")
 
         return data
 
-if __name__ == "__main__":
-
-    audio_file1 = "A:/MLProjs/PR09-tds/input/3.ogg"
-    audio_file2 = "A:/MLProjs/PR09-tds/input/1.ogg"
-
-    t = Transcriber()
-
-    # Строка вызова
-    print(t.transcribe_audio(audio_path=audio_file1))
+# if __name__ == "__main__":
+#
+#     audio_file1 = "A:/MLProjs/PR09-tds/input/3.ogg"
+#     audio_file2 = "A:/MLProjs/PR09-tds/input/1.ogg"
+#
+#     t = Transcriber()
+#
+#     # Строка вызова
+#     print(t.transcribe_audio(audio_path=audio_file1))
