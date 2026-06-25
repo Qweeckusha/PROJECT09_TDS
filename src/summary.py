d2fd26b8495c66b7af26582837353645f67242c3
qweeck <tolya.zaytsev.2004@gmail.com>
2026-06-25 15:25:36 +1000

    new func with LLM, update old funcs for LLM

--- src/summary.py
+++ src/summary.py
@@ -1,11 +1,13 @@
-from datetime import datetime
-
-from transcriber import Transcriber
-from diarizer import CustomDiarizer
-
 import os
+from datetime import datetime
+from pathlib import Path
 
-from config import FULL_TEXT_DIR, save_full_text_file
+from config import FULL_TEXT_DIR, save_full_text_file, save_tds_file, TDS_DIR, prompt, model_name
+from diarizer import CustomDiarizer
+from transcriber import Transcriber
+
+import ollama as ol
+
 
 class Summarizer:
     def __init__(self):
@@ -13,9 +15,15 @@ class Summarizer:
         self.diarizer = CustomDiarizer(token=os.getenv("HF_TOKEN"))
 
     def _align_segments(self, whisper_seg: list[dict], diarizer_seg: list[dict]) -> list[dict]:
+        """
+        Ключевая функция агрегации для соединения двух списков словарей от разных модулей
 
+        :param whisper_seg: сегменты от модуля транскрибирования
+        :param diarizer_seg: сегменты от модуля диаризации
+        :return: список словарей по каждому отрезку транскрибирования, где добавляется соответствующий спикер
+        """
 
-        # Сценарий A: модуль транскрибации не смог определить текст в аудио или вернул ошибку.
+        # Сценарий A: модуль транскрибирования не смог определить текст в аудио или вернул ошибку.
         if not whisper_seg:
             return []
 
@@ -62,7 +70,14 @@ class Summarizer:
         return result
 
     def _format_full_text(self, align_segs: list[dict]) -> str:
+        """
+        Форматирует текст до читаемого вида и проверяет на монолог или диалог+
 
+        При монологе возвращает текст без спикеров, только временные метки
+
+        :param align_segs: align_segments - список объединённых сегментов
+        :return: полный текст прошедший стандартный pipeline
+        """
         if not align_segs:
             return "ERROR: _align_segments return did nothing"
 
@@ -84,28 +99,86 @@ class Summarizer:
         return "\n".join(lines)
 
 
-    def get_full_text_from_audio(self, path: str):
+    def get_full_text_from_audio(self, path: str) -> str:
+        """
+        Стандартный Pipeline (StdPl) - задействует все модули и прогоняет файл через них
 
-        whisper_seg = self.transcriber.transcribe_audio(path)
-        diarizer_seg = self.diarizer.build_diarize_to_list(path)
+        :param path: путь к аудиофайлу
+        :return: возвращает полный текст транскрибированный и диаризованный
+        """
+
+        try:
+            whisper_seg = self.transcriber.transcribe_audio(path)
+            diarizer_seg = self.diarizer.build_diarize_to_list(path)
+        except Exception as e:
+            print(e)
+            return "ERROR: Не удалось выполнить стандартный Pipeline. ADDR: summary.get_full_text_from_audio"
 
         align = self._align_segments(whisper_seg, diarizer_seg)
         text = self._format_full_text(align)
         if save_full_text_file:
-            self._save_full_text(text)
+            self._save_text(path, text)
         return text
 
-    def _save_full_text(self, text: str):
+    def _save_text(self, path: str, text: str, isLLM: bool = False) -> None:
+        """
+        Сохраняет текст в кэш в виде .txt файла
+
+        :param path: путь к аудиофайлу, но нужен для названия output файлов
+        :param text: текст для записи в файл
+        :param isLLM: bool переменная для разделения по директориям
+        :return: None возвращать нечего, по сути функция просто выполняет свою задачу и не обязана отчитываться
+        """
 
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        file_path = FULL_TEXT_DIR / f"saved_{timestamp}.txt"
+        source_name = Path(path).stem
+        if isLLM:
+            TDS_DIR.mkdir(exist_ok=True)
+            file_path = TDS_DIR / f"TDS-audio_{source_name}_{timestamp}.txt"
+        else:
+            FULL_TEXT_DIR.mkdir(exist_ok=True)
+            file_path = FULL_TEXT_DIR / f"transcribed-audio_{source_name}_{timestamp}.txt"
 
         file_path.write_text(text, encoding="utf-8")
-        return file_path
+        return None
 
-    def get_text_from_llm(self, path: str):
-        pass
+    def get_text_from_llm(self, model_name: str, path: str) -> str:
+        """
+        Полный Pipeline (FPl) - выполняет конечную роль - суммаризация текста, определение выводов и важных тезисов при помощи LLM
+
+        :param model_name: переменная с точным названием модели, должна быть предварительно скачана
+        :param path: Путь к аудиофайлу - первая точка вхождения
+        :return: результат вызова LLM с промптом и текстом из StdPl
+        """
+
+        audio_text = self.get_full_text_from_audio(path)
 
 
-text = Summarizer().get_full_text_from_audio("A:/MLProjs/PR09-tds/input/3.ogg")
+        try:
+            response: ol.ChatResponse = ol.chat(model=model_name,
+                                                    messages=[
+                                                        {
+                                                            "role": "system",
+                                                            "content": prompt
+                                                        },
+                                                        {
+                                                            "role": "user",
+                                                            "content": audio_text
+                                                        }
+                                                    ])
+            llm_output = response.message.content
+        except Exception as e:
+            print(f'Ошибка при работе с LLM: [{e}]')
+            return "ERROR: не удалось получить саммари от LLM. ADDR: summary.get_text_from_llm"
+
+        if save_tds_file:
+            self._save_text(path, llm_output, isLLM=True)
+
+        return llm_output
+
+
+
+
+
+text = Summarizer().get_text_from_llm(model_name=model_name, path="A:/MLProjs/PR09-tds/input/3.ogg")
 print(text)
